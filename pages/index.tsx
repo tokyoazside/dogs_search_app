1eb78130b527beaa5a933b4ce2b8c8ac838ceaff
osamu <tokyo.azside@gmail.com>
2022-09-28 17:15:17 +0900

    bootStorap 見栄えをよろしくした

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,11 +45,75 @@ const hundleClick = async () => {
       height: "100vh"
         
     }} >
-      {/* <button className="fetchButton" onClick={hundleClick}>今日のわんこ</button> */}
-      <button className="btn btn-primary" onClick={hundleClick}>今日のわんこ</button>
-      <img src={catImgUrl} width="500" height="auto" className="catImg" />
 
-    </div>
+<nav className="navbar navbar-expand-lg navbar-light bg-dark py-3 mt-5">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            {/* <h2 className="text-white  mb-2 " >Dog Gallary</h2> */}
+            <h2 className="text-white" >Dog Gallary</h2>
+          </a>
+
+{/*         
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Services</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Projects</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Team</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Blog</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Contact</a>
+            </li>
+            </ul>
+            <button className="btn btn-primary ms-2">Join Us</button>
+
+          </div> */}
+        </div>
+</nav>
+{/* <div className="dog vh-100 d-flex align-items-center pt-5"  id="home"> */}
+<div className="dog vh-100 d-flex pt-5"  id="home">
+        <div className="container-fluid">
+         <div className="row">
+            <div className="col-4 ">
+                    {/* <button className="fetchButton" onClick={hundleClick}>今日のわんこ</button> */}
+             <div className="button  text-center">
+                <button className="btn btn-primary" onClick={hundleClick}>今日のわんこ</button>
+
+             </div>
+ 
+            </div>
+            <div className="col-8 dogImg">
+              {/* <img src={catImgUrl} width="500" height="auto" className="catImg" /> */}
+              <img src={catImgUrl} className="catImg" />
+            </div>
+        </div>
+
+      {/* <!-- hero section -->
+    <div className=""  id="home">
+        <div className="container">
+
+      </div>    </div>
+        </div>*/}
+        </div> 
+      </div>
+      </div>
   )
 }
 
